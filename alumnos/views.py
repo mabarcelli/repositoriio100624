d7b885867dc94661ff0610524c0da4c0cd2ada89
unknown <SSDD@PNAWSA06LC1002.duoc.acad>
2024-06-10 18:17:28 -0400

    first comit

---
+++ alumnos/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .models import Genero , Alumno
+
+# Create your views here.
+def index(request):
+    alumnos = Alumno.objects.all()
+    context={"alumnos":alumnos}
+    return render(request,"alumnos/index.html",context)
+
+
+
+def crud (request):
+    alumnos = Alumno.objects.all()
+    context={"alumnos":alumnos}
+    return render (request,"alumnos/alumnos_list.html", context)
+
+def alumnosAdd (request):
+    return "hola"
+
+def alumnos_del (request,pk):
+    return "hola"
+
+def alumnos_findEdit(request,pk):
+    return "hola"
